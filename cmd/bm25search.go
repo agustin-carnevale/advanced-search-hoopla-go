740b3b01d3add108bbca6668ff565e9316bd8497
Agustin Carnevale <agustinc.dev@gmail.com>
2025-11-10 21:17:07 -0300

    bm25 search implementation

---
+++ cmd/bm25search.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/agustin-carnevale/advanced-search-hoopla-go/internal/index"
+	"github.com/spf13/cobra"
+)
+
+// bm25searchCmd represents the bm25search command
+var bm25searchCmd = &cobra.Command{
+	Use:   "bm25search query <limit>",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("❌ Please provide a query to search for.")
+			return
+		}
+		query := args[0]
+		limit := 5
+		if len(args) > 1 {
+			limitString := args[1]
+			l, err := strconv.Atoi(limitString)
+			if err != nil {
+				log.Fatalf("❌ limit should be a int: %v\n", err)
+				return
+			}
+			limit = l
+		}
+
+		// load index
+		idx := index.NewInvertedIndex()
+		if err := idx.Load(); err != nil {
+			log.Fatalf("❌ Failed to load index: %v\n", err)
+		}
+
+		results := idx.Bm25Search(query, limit)
+
+		for i, doc := range results {
+			fmt.Printf("%d. (%d) %s - Score: %.2f\n", i+1, doc.DocID, doc.Movie.Title, doc.Score)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bm25searchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// bm25searchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// bm25searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- internal/index/inverted_index.go
+++ internal/index/inverted_index.go
@@ -7,11 +7,18 @@ import (
 	"math"
 	"os"
 	"slices"
+	"sort"
 
 	"github.com/agustin-carnevale/advanced-search-hoopla-go/internal/fs"
 	"github.com/agustin-carnevale/advanced-search-hoopla-go/internal/model"
 	"github.com/agustin-carnevale/advanced-search-hoopla-go/internal/tokenizer"
 )
+
+type SearchResult struct {
+	DocID int
+	Score float64
+	Movie model.Movie // optional: include your movie struct here if needed
+}
 
 type InvertedIndex struct {
 	Index           map[string]map[int]struct{} // term -> set of doc IDs
@@ -169,6 +176,47 @@ func (idx *InvertedIndex) GetBM25TF(docID int, term string, k1 float64, b float6
 	bm25tf := (tf * (k1 + 1)) / (tf + k1*lengthNorm)
 
 	return bm25tf
+}
+
+func (idx *InvertedIndex) bm25Score(docID int, term string) float64 {
+	bm25idf := idx.GetBM25IDF(term)
+	bm25tf := idx.GetBM25TF(docID, term, 1.5, 0.75)
+
+	return bm25idf * bm25tf
+}
+
+func (idx *InvertedIndex) Bm25Search(query string, limit int) []SearchResult {
+	stopWords, err := fs.LoadStopWords()
+	if err != nil {
+		log.Fatalf("Error loading stop words: could not tokenize term.")
+	}
+	qTokens := tokenizer.Tokenize(query, stopWords)
+
+	// Preallocate results slice directly
+	results := make([]SearchResult, 0, len(idx.DocMap))
+	for docID, movie := range idx.DocMap {
+		var score float64
+		for _, t := range qTokens {
+			score += idx.bm25Score(docID, t)
+		}
+
+		results = append(results, SearchResult{
+			DocID: docID,
+			Score: score,
+			Movie: movie,
+		})
+	}
+
+	// Sort by Score DESC
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Score > results[j].Score
+	})
+
+	if limit < len(results) {
+		results = results[:limit]
+	}
+
+	return results
 }
 
 func (idx *InvertedIndex) Build() error {
